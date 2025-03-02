c709f4d0e2c7491dd567d9d6ec9a312785e137a2
Ezequiel Garibotti <ezgaribotti@gmail.com>
2025-03-02 18:56:52 -0300

    Using the repository pattern

---
+++ app/Repositories/Repository.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Entities\Entity;
+use App\Interfaces\RepositoryInterface;
+
+abstract class Repository implements RepositoryInterface
+{
+    public function __construct(protected Entity $entity)
+    {
+    }
+
+    public function all(): object
+    {
+        return $this->entity->all();
+    }
+
+    public function paginate(): object
+    {
+        return $this->entity->simplePaginate(15);
+    }
+
+    public function find($id): ?object
+    {
+        return $this->entity->find($id);
+    }
+
+    public function findOrFail($id): object
+    {
+        return $this->entity->findOrFail($id);
+    }
+
+    public function create(array $attributes): object
+    {
+        return $this->entity->create($attributes);
+    }
+
+    public function update(array $attributes, $id): void
+    {
+        $this->entity->findOrFail($id)->update($attributes);
+    }
+
+    public function delete($id): void
+    {
+        $this->entity->findOrFail($id)->delete();
+    }
+}
--- app/helpers.php
+++ app/helpers.php
@@ -1,8 +1,17 @@
 <?php
 
+use Illuminate\Support\Fluent;
+
 if (! function_exists('build_path')) {
     function build_path(string ...$segments): string
     {
         return implode(DIRECTORY_SEPARATOR, $segments);
+    }
+}
+
+if (! function_exists('to_object')) {
+    function to_object(array $attributes = []): object
+    {
+        return new Fluent($attributes);
     }
 }
